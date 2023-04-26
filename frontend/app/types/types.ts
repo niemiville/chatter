d58db227aad66e05fe42e1f0036c19758a606446
Ville Niemi <vilsuxn@gmail.com>
2023-04-26 06:22:17 +0300

    moved all types from components to type file

--- frontend/app/types/types.ts
+++ frontend/app/types/types.ts
@@ -4,7 +4,12 @@ export interface Contact {
     status: string;
     createdAt: string;
     updatedAt: string;
-}
+};
+
+export interface ContactNames {
+    id: number;
+    username: string;
+};
 
 export interface User {
     id: number;
@@ -12,4 +17,25 @@ export interface User {
     email: string;
     roles: [string];
     accessToken: string;
-}
+};
+
+export interface Message{
+    id: number;
+    senderId: number;
+    receiverId: number;
+    body: string;
+    createdAt: string;
+    updatedAt: string;
+};
+
+export interface SignInFormValues {
+    username: string;
+    password: string;
+};
+
+export interface SignUpFormValues {
+    username: string;
+    password: string;
+    email: string;
+    roles: [string];
+};
